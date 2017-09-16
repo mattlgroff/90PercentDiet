36a55705f4d2945cde825dcceba3ea2e055dbc9a
Matt Groff <mattlgroff@gmail.com>
2017-09-16 11:53:16 -0700

    Express and Request only

---
+++ public/assets/javascript/app.js
@@ -0,0 +1,40 @@
+// var usdaApiKey = "iBklKDIgJEc6JVRhYE3OX7AvpEChxD1953KbPgSl";
+// var nbdno = 01009;
+// var usdaUrl = "https://api.nal.usda.gov/ndb/reports/";
+
+// $.ajax({
+//     url: usdaUrl,
+//     data: { 
+//         "api_key": usdaApiKey, 
+//         "type": "b", 
+//         "format": "json",
+//         "nbdno": 01009
+//     },
+//     type: "GET",
+//     success: function(response) {
+//         console.log(response);
+//     },
+//     error: function(error) {
+//         console.log(error);
+//     }
+// });
+
+
+
+
+// var fatsecretApiKey = "399d02dfa89d47b28552a3dee945debe";
+// var fatsecretUrl = "http://platform.fatsecret.com/js";
+
+// $.ajax({
+//     url: fatsecretUrl,
+//     data: { 
+//         "key": fatsecretApiKey
+//     },
+//     type: "GET",
+//     success: function(response) {
+//         console.log(response);
+//     },
+//     error: function(error) {
+//         console.log(error);
+//     }
+// });
