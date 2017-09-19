d0a0142ca4cd94eecae334eb38d29499204a60b2
Matt Groff <mattlgroff@gmail.com>
2017-09-18 17:19:53 -0700

    Just hosting now.

--- 90percent.js
+++ 90percent.js
@@ -1,7 +1,7 @@
 //Require Request and Express.
 const request = require('request');
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 //Express
 var app = express();
@@ -10,139 +10,140 @@ app.use(express.static(__dirname + '/public')) // make express look in the publi
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port)
 });
-app.use(bodyParser());
-//
-app.post('/send', function(request, response){
+// app.use(bodyParser());
 
-  
-  console.log("Request: " + request.body.content);
+// app.post('/send', function(request, response){
+//   const searchTerm = request.body.content;
 
-  //Take user input from the Ajax Post Request from the search box
-  var searchTerm = request.body.content;
+//   searchUsdaApi(searchTerm)
+//   .then(data => {
+//     console.log(data);
 
-  //Send it to our Search USDA Api 
-  console.log( searchUsdaApi(searchTerm) );
-  // searchUsdaApi(searchTerm).then(function(data){
-  //   console.log("searchTermdata: " + data);
+//      var resultObj = {
+//       content: data
+//     }
 
-  //   response.json({
-  //     content: data
-  //   });
-  // })
+//     response.json(resultObj)
+//   })
+// });
 
 
-});
+// function searchUsdaApi (searchTerm) {
+//   // let resultsArray = [];
+//   // const results = 5;
+//   var usdaApiKey = "iBklKDIgJEc6JVRhYE3OX7AvpEChxD1953KbPgSl",
+//   usdaSearchUrl = "https://api.nal.usda.gov/ndb/search/",
+//   searchUsda = {
+//     url: usdaSearchUrl,
+//     method: 'get',
+//     qs: {
+//       'api_key': usdaApiKey,
+//       'q': searchTerm,
+//       'format': 'json',
+//       'sort': 'n',
+//       'max': results
+//     }
+//   };
 
+//   return searchApiRequest(searchUsda)
+//   .then(data => {
+//     console.log("Inside searchApiRequest promise");
 
-function searchUsdaApi(searchTerm){
-  var query = searchTerm;
-  var results = 1;
-  var resultsArray = [];
+//     //var resultsArray = [];
+//     //let promises = []
 
-  var usdaApiKey = "iBklKDIgJEc6JVRhYE3OX7AvpEChxD1953KbPgSl",
-    usdaSearchUrl = "https://api.nal.usda.gov/ndb/search/",
-    searchUsda = {
-      method: 'get',
-      qs: {
-        'api_key': usdaApiKey,
-        'q': query,
-        'format': "json",
-        'sort': "n",
-        'max': results
-      },
-      url: usdaSearchUrl
-    };
+//     //for (let i = 0; i < results; i++) {
 
-  searchApiRequest(searchUsda).then(function(data){
+//       var name = data.item[0].name;
 
-    console.log("Inside searchApiRequest promise");
+//       // promises.push(usdaFoodLookup(data.item[i]))
+//       var results = usdaFoodLookup(data.item[0].ndbno).then(function(body){
 
-    var resultsArray = [];
+//         console.log("Inside usdaFoodLookup promise= " + 0);
 
-    for (var i = 0; i < results; i++){
+//         var nutrient1 = body.nutrients[0].unit + " of " + body.nutrients[0].name + " in 100g: " + body.nutrients[0].value;
+//         var nutrient2 = body.nutrients[1].unit + " of " + body.nutrients[1].name + " in 100g: " + body.nutrients[1].value;
 
-      var name = data.item[i].name;
+//         var results = { "Name": name, "Nutrient1": nutrient1, "Nutrient2": nutrient2 };
 
-      usdaFoodLookup(data.item[i].ndbno).then(function(body){
+//         return results;
 
-        console.log("Inside usdaFoodLookup promise");
-
-        var nutrient1 = body.nutrients[0].unit + " of " + body.nutrients[0].name + " in 100g: " + body.nutrients[0].value;
-        var nutrient2 = body.nutrients[1].unit + " of " + body.nutrients[1].name + " in 100g: " + body.nutrients[1].value;
-
-        resultsArray[i] = { "Name": name, "Nutrient1": nutrient1, "Nutrient2": nutrient2 };
-        console.log(resultsArray[i])
-
-      })
-      .catch(function(error){
-        console.log("Error: " + error);
-      }); //End usdaFoodLookup Promise
+//       })
+//       .catch(function(error){
+//         console.log("Error: " + error);
+//       }); //End usdaFoodLookup Promise
       
-    }
+//       return results;
+//       //promises.push(promise)
+      
+//     //}
+//     //I am dumb and this won't work. Not async ready
+//     // return Promise.all(promises).then(function(){ 
+//     //   console.log(resultsArray);
+//     // });
+//     // thing().then(resultsArray => {})
+//     //return resultsArray;
 
-    return resultsArray;
+//   }) //End searchRequestPromise
 
-  }) //End searchRequestPromise
+//   .catch(function(error){
+//     console.log("Error: " + error);
+//    });
 
-  .catch(function(error){
-    console.log("Error: " + error);
-   });
+// }
 
-}
+// function usdaFoodLookup(foodNumber){
+//   //USDA Info
+//   var usdaApiKey = "iBklKDIgJEc6JVRhYE3OX7AvpEChxD1953KbPgSl",
+//       usdaUrl = "https://api.nal.usda.gov/ndb/reports/",
+//       usdaAuth = {
+//               method: 'get',
+//               qs: {
+//                 'api_key': usdaApiKey,
+//                 'type': "b",
+//                 'format': "json",
+//                 'ndbno': foodNumber
+//               },
+//               url: usdaUrl
+//   };    
 
-
-function usdaFoodLookup(foodNumber){
-  //USDA Info
-  var usdaApiKey = "iBklKDIgJEc6JVRhYE3OX7AvpEChxD1953KbPgSl",
-      usdaUrl = "https://api.nal.usda.gov/ndb/reports/",
-      usdaAuth = {
-              method: 'get',
-              qs: {
-                'api_key': usdaApiKey,
-                'type': "b",
-                'format': "json",
-                'ndbno': foodNumber
-              },
-              url: usdaUrl
-  };    
-
-  return foodLookupApiRequest(usdaAuth);   
+//   return foodLookupApiRequest(usdaAuth);   
   
-}
+// }
 
-function searchApiRequest(auth){
-  return new Promise(function(resolve, reject){
+// function searchApiRequest(auth){
+//   return new Promise(function(resolve, reject){
 
-      request(auth, function(err, res, body){
-          // in addition to parsing the value, deal with possible errors
-          if (err) return reject(err);
+//       request(auth, function(err, res, body){
+//           // in addition to parsing the value, deal with possible errors
+//           if (err) return reject(err);
 
-          try {
-              // JSON.parse() can throw an exception if not valid JSON
-              resolve(JSON.parse(body).list);
-          } catch(e) {
-              reject(e);
-          }
-      });
-  });
-}
+//           try {
+//               // JSON.parse() can throw an exception if not valid JSON
+//               resolve(JSON.parse(body).list);
+//           } catch(e) {
+//               reject(e);
+//           }
+//       });
+//   });
+// }
 
-function foodLookupApiRequest(auth){
-  return new Promise(function(resolve, reject){
+// function foodLookupApiRequest(auth){
+//   return new Promise(function(resolve, reject){
 
-      request(auth, function(err, res, body){
-          // in addition to parsing the value, deal with possible errors
-          if (err) return reject(err);
+//       request(auth, function(err, res, body){
+//           // in addition to parsing the value, deal with possible errors
+//           if (err) return reject(err);
 
-          try {
-              // JSON.parse() can throw an exception if not valid JSON
-              resolve(JSON.parse(body).report.food);
-          } catch(e) {
-              reject(e);
-          }
-      });
-  });
-}
+//           try {
+//               // JSON.parse() can throw an exception if not valid JSON
+//               resolve(JSON.parse(body).report.food);
+//           } catch(e) {
+//               reject(e);
+//           }
+//       });
+//   });
+// }
 
 
 
