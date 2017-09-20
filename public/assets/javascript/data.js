3f6617512531666315223942d6f4343676e49c99
Matt Groff <mattlgroff@gmail.com>
2017-09-19 18:48:19 -0700

    Added scatterplot and changed Jumbotron

--- public/assets/javascript/data.js
+++ public/assets/javascript/data.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 
     database.ref("/").on("value", function(snapshot) {
 
+      //Gender Split Graph
       var maleDb = snapshot.child("male");
       var male = maleDb.numChildren();
       console.log("Male users: " + male);
@@ -31,13 +32,39 @@ $(document).ready(function(){
       var percentMale = (male / totalUsers) * 100;
 
       displayGenderHighchart(percentMale,percentFemale);
+
+      //Scatter Plot
+      var maleArray = [];
+      database.ref("/male").orderByChild("dateAdded").on("value", function(snapshot) {
+
+        $.each(snapshot.val(), function(index, value) {
+            var x = value['Height(cm)'];
+            var y = value['Weight(kg)'];
+            maleArray.push([x,y]);
+        }); 
+
+      }); //End Male Ref
+
+      var femaleArray = [];
+      database.ref("/female").orderByChild("dateAdded").on("value", function(snapshot) {
+
+        $.each(snapshot.val(), function(index, value) {
+            var x = value['Height(cm)'];
+            var y = value['Weight(kg)'];
+            femaleArray.push([x,y]);
+        }); 
+
+      }); //End Female Ref
+
+      displayScatterplot(maleArray,femaleArray);
+
     });
 
 
 });
 
 function displayGenderHighchart(percentMale,percentFemale){
-        //Highcharts
+        //Highcharts Pie Chart
         Highcharts.chart('genderChart', {
             chart: {
                 plotBackgroundColor: null,
@@ -77,4 +104,79 @@ function displayGenderHighchart(percentMale,percentFemale){
                 }]
             }]
         });
-    }
+}
+
+function displayScatterplot(maleArray, femaleArray){
+    Highcharts.chart('scatterPlot', {
+      chart: {
+          type: 'scatter',
+          zoomType: 'xy'
+      },
+      title: {
+          text: 'Height Versus Weight of Project90Percent Users'
+      },
+      subtitle: {
+          text: 'Source: Project90Percent'
+      },
+      xAxis: {
+          title: {
+              enabled: true,
+              text: 'Height (cm)'
+          },
+          startOnTick: true,
+          endOnTick: true,
+          showLastLabel: true
+      },
+      yAxis: {
+          title: {
+              text: 'Weight (kg)'
+          }
+      },
+      legend: {
+          layout: 'vertical',
+          align: 'left',
+          verticalAlign: 'top',
+          x: 100,
+          y: 70,
+          floating: true,
+          backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+          borderWidth: 1
+      },
+      plotOptions: {
+          scatter: {
+              marker: {
+                  radius: 5,
+                  states: {
+                      hover: {
+                          enabled: true,
+                          lineColor: 'rgb(100,100,100)'
+                      }
+                  }
+              },
+              states: {
+                  hover: {
+                      marker: {
+                          enabled: false
+                      }
+                  }
+              },
+              tooltip: {
+                  headerFormat: '<b>{series.name}</b><br>',
+                  pointFormat: '{point.x} cm, {point.y} kg'
+              }
+          }
+      },
+      series: [{
+          name: 'Female',
+          color: 'rgba(223, 83, 83, .5)',
+          data: femaleArray
+
+      }, {
+          name: 'Male',
+          color: 'rgba(119, 152, 191, .5)',
+          data: maleArray
+      }]
+  });
+
+
+}
